f16684516cfde70baed3decec11be72f155d406f
Tim van Meijel <t.m.m.v.meijel@student.tue.nl>
2023-06-05 18:27:09 +0200

    new figures

--- piezo_sim_first_trials_TimM/piezo_example.m
+++ piezo_sim_first_trials_TimM/piezo_example.m
@@ -22,7 +22,7 @@ clear
 % If not, how can you ensure good tracking performance on the experimental 
 % setup if the piezo behaves like case (b)? 
 
-use_monotone_function = true;
+use_monotone_function = false;
 
 
 %% Set Piezo parameters
@@ -35,7 +35,7 @@ end
 piezo_gain = 1e-7;
 
 % load("time_array.mat")
-Ntrials = 4;
+Ntrials = 11;
 %% Simulate Piezo
 % error = [out.error.Time zeros(4011,1)];
 % % Edit the simulink file to include your ILC/RC implementation
@@ -62,12 +62,15 @@ for N = 1:Ntrials
     error_dat = out.error.Data;
 %     error_dat( abs(error_dat) < 10^(-10)) = 10^(-8);
 %     f_jplus1_dat = f_j(:,2) - (1./error_dat)*piezo_gain;
-    f_jplus1_dat = f_j(:,2) - error_dat/piezo_gain;
+    f_jplus1_dat = f_j(:,2) - 0.5*error_dat/piezo_gain;
+%     f_jplus1_dat = f_j(:,2) - error_dat/piezo_gain;
     f_jplus1 = [time' f_jplus1_dat];
 
     f_j_save(:,N) = f_j(:,2);
     error_save(:,N) = error_dat;
     alpha_save(:,N) = out.alpha_mod.Data;
+    ref_save(:,N) = out.refout.Data;
+    y_save(:,N) = out.y.Data;
 end
 
 
@@ -77,11 +80,11 @@ alpha = 0:2*pi/1000:2*pi;
 close all;
 figure
 hold on
-for N = 1:Ntrials
+for N = 1:2:Ntrials
 plot(alpha,f_j_save(1:1001,N))
 end
 title('Feedforward signal \alpha over iterations')
-legend('Trial 1','Trial 2','Trial 3','Trial 4',Location='best')
+legend('Trial 0','Trial 2','Trial 4','Trial 6','Trial 8','Trial 10',Location='best')
 xlabel('\alpha [rad]')
 ylabel('Feedforward signal \alpha [rad]')
 xlim([0 2*pi])
@@ -91,11 +94,11 @@ xticklabels({'0', '\pi', '2\pi'})
 
 figure
 hold on
-for N = 1:Ntrials
+for N = 1:2:Ntrials
 plot(alpha,error_save(1:1001,N))
 end
 title('Error over iterations')
-legend('Trial 1','Trial 2','Trial 3','Trial 4',Location='best')
+legend('Trial 0','Trial 2','Trial 4','Trial 6','Trial 8','Trial 10',Location='best')
 xlabel('\alpha [rad]')
 ylabel('Error [m]')
 xlim([0 2*pi])
@@ -105,18 +108,18 @@ xticklabels({'0', '\pi', '2\pi'})
 
 figure
 hold on
-for N = 1:Ntrials
+for N = 1:2:Ntrials
 plot(error_save(1:1001,N).*10^7,'r')
 plot(f_j_save(1:1001,N),'k')
 end
 
 figure
 hold on
-for N = 1:Ntrials
+for N = 1:2:Ntrials
 plot(alpha,alpha_save(1:1001,N))
 end
 title('Input \alpha over iterations')
-legend('Trial 1','Trial 2','Trial 3','Trial 4',Location='best')
+legend('Trial 0','Trial 2','Trial 4','Trial 6','Trial 8','Trial 10',Location='best')
 xlabel('\alpha [rad]')
 ylabel('\alpha [rad]')
 xlim([0 2*pi])
@@ -125,7 +128,7 @@ xticklabels({'0', '\pi', '2\pi'})
 
 
 %% 
-figure(1); clf;
+figure
 subplot(2,1,1)
 plot(out.alpha_mod)
 ylim([0 2*pi])
@@ -136,6 +139,26 @@ grid on
 
 subplot(2,1,2)
 plot(out.y)
+title('Piezo position')
+ylabel('Position [m]')
+xlabel('Time [s]')
+grid on
+
+figure
+subplot(2,1,1)
+plot(alpha_save(:,1))
+hold on
+plot(alpha_save(:,N))
+ylim([0 2*pi])
+title('Commutation angle')
+ylabel('Alpha [rad]')
+xlabel('Time [s]')
+grid on
+
+subplot(2,1,2)
+plot(y_save(:,1))
+hold on
+plot(y_save(:,N))
 title('Piezo position')
 ylabel('Position [m]')
 xlabel('Time [s]')
